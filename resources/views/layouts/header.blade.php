220f759bb562458bd0b6f668dd2bfad001db4ab2
kadzus1 <knowicka@interia.pl>
2024-06-20 19:46:27 +0200

    Initial commit

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -88,68 +88,6 @@
 
         <!-- Main -->
 
-        <!-- Footer -->
-        <section id="footer">
-            <div class="container">
-                <header>
-                    <h2>Pytanie lub komentarz? <strong>Jesteśmy w kontakcie:</strong></h2>
-                </header>
-                <div class="row">
-                    <div class="col-6 col-12-medium">
-                        <section>
-                            <form method="post" action="#">
-                                <div class="row gtr-50">
-                                    <div class="col-6 col-12-small">
-                                        <input name="name" placeholder="Imię" type="text" />
-                                    </div>
-                                    <div class="col-6 col-12-small">
-                                        <input name="email" placeholder="Email" type="text" />
-                                    </div>
-                                    <div class="col-12">
-                                        <textarea name="message" placeholder="Wiadomość"></textarea>
-                                    </div>
-                                    <div class="col-12">
-                                        <a href="#" class="form-button-submit button icon solid fa-envelope">Wyślij wiadomość</a>
-                                    </div>
-                                </div>
-                            </form>
-                        </section>
-                    </div>
-                    <div class="col-6 col-12-medium">
-                        <section>
-                            <div class="row">
-                                <div class="col-6 col-12-small">
-                                    <ul class="icons">
-                                        <li class="icon solid fa-home">
-                                            1234 Będzińska<br />
-                                            Sosnowiec, 41-205<br />
-                                            Polska
-                                        </li>
-                                        <li class="icon solid fa-phone">
-                                            (000) 000-0000
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="col-6 col-12-small">
-                                    <ul class="icons">
-                                        <li class="icon solid fa-envelope">
-                                            <a href="#">biuro@admin.pl</a>
-                                        </li>
-                                        <li class="icon brands fa-instagram">
-                                            <a href="#">instagram.com/untitled</a>
-                                        </li>
-                                        <li class="icon brands fa-facebook-f">
-                                            <a href="#">facebook.com/untitled</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </section>
-                    </div>
-                </div>
-            </div>
-        </section>
-
         
 
     </div>
